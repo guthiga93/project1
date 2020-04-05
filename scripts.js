975135e19068a41a7d45b9a9992ee6b1240fb65e
antony guthiga <toniguthiga93@gmail.com>
2020-04-06 00:29:59 +0300

    Add commit

---
+++ scripts.js
@@ -0,0 +1,441 @@
+var production = {
+    shedA:{
+      noOfCows: 34,
+      averageProduction: 15
+    }
+  };
+  function shedA(noOfCows,averageProduction){
+    console.log(noOfCows * averageProduction);
+  }
+  shedA(34,15);
+  
+  var production = {
+    shedB:{
+      noOfCows: 11,
+      averageProduction: 28
+    }
+  };
+  function shedB(noOfCows,averageProduction){
+    console.log(noOfCows * averageProduction);
+  }
+  shedB(11,28);
+  
+  var production = {
+    shedC:{
+      noOfCows: 27,
+      averageProduction: 18
+    }
+  };
+  function shedC(noOfCows,averageProduction){
+    console.log(noOfCows * averageProduction);
+  }
+  shedC(27,18);
+  
+  var production = {
+    shedD:{
+      noOfCows: 52,
+      averageProduction: 11
+    }
+  };
+  function shedD(noOfCows,averageProduction){
+    console.log(noOfCows * averageProduction);
+  }
+  shedD(52,11);
+  
+  function totalProduction(shedA,shedB,shedC,shedD){
+    console.log(shedA + shedB + shedC + shedD);
+  }
+  totalProduction(510,308,486,572);
+  
+  function incomeOverTime(selling_price,time,totalProduction){
+    console.log(selling_price * time * totalProduction);
+  }
+  incomeOverTime(45,7,1876);
+  incomeOverTime(45,365,1876);
+  
+  var income = {
+    monthJanuary:{
+      days: 31,
+    totalProduction: 1876,
+    rate: 45
+    }
+  };
+  function monthJanuary(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthJanuary(31,1876,45);
+  
+  var income = {
+    monthFebruary:{
+      days: 29,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthFebruary(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthFebruary(29,1876,45);
+  
+  var income = {
+    monthMarch:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthMarch(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthMarch(31,1876,45);
+  
+  var income = {
+    monthApril:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthApril(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthApril(30,1876,45);
+  
+  var income = {
+    monthMay:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthMay(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthMay(31,1876,45);
+  
+  var income = {
+    monthJune:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthJune(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthJune(30,1876,45);
+  
+  var income = {
+    monthJuly:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthJuly(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthJuly(31,1876,45);
+  
+  var income = {
+    monthAugust:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthAugust(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthAugust(31,1876,45);
+  
+  var income = {
+    monthSeptember:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthSeptember(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthSeptember(30,1876,45);
+  
+  var income = {
+    monthOctober:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthOctober(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthOctober(31,1876,45);
+  
+  var income = {
+    monthNovember:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthNovember(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthNovember(30,1876,45);
+  
+  var income = {
+    monthDecember:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 45
+    }
+  };
+  function monthDecember(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  monthDecember(31,1876,45);
+  
+  var income = {
+    monthJanuary:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforJanuaryis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforJanuaryis(31,1876,49.60);
+  
+  var income = {
+    monthFebruary:{
+      days: 29,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforFebruaryis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforFebruaryis(29,1876,49.60);
+  
+  var income = {
+    monthMarch:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforMarchis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforMarchis(31,1876,49.60);
+  
+  var income = {
+    monthApril:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforAprilis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforAprilis(30,1876,49.60);
+  
+  var income = {
+    monthMay:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforMayis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforMayis(31,1876,49.60);
+  
+  var income = {
+    monthJune:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforJuneis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforJuneis(30,1876,49.60);
+  
+  var income = {
+    monthJuly:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforJulyis(days,totalProduction,rate){
+    console.log(days* totalProduction * rate);
+  }
+  YourincomeforJulyis(31,1876,49.60);
+  
+  var income = {
+    monthAugust:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforAugustis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforAugustis(31,1876,49.60);
+  
+  var income = {
+    monthSeptember:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforSeptemberis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforSeptemberis(30,1876,49.60);
+  
+  var income = {
+    monthOctober:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforOctoberis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforOctoberis(31,1876,49.60);
+  
+  var income = {
+    monthNovember:{
+      days: 30,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforNovemberis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforNovemberis(30,1876,49.60);
+  
+  var income = {
+    monthDecember:{
+      days: 31,
+      totalProduction: 1876,
+      rate: 49.60
+    }
+  };
+  function YourincomeforDecemberis(days,totalProduction,rate){
+    console.log(days * totalProduction * rate);
+  }
+  YourincomeforDecemberis(31,1876,49.60);
+  
+  var comparison ={
+    monthJanuary:{
+      newrate: 2884537.6,
+      oldrate: 2617020
+    }
+  };
+  function differenceinrate(newrate,oldrate){
+    console.log(Math.round(newrate-oldrate));
+  }
+  differenceinrate(2884537.6,2617020);
+  
+  var comparison ={
+    monthFebruary:{
+      newate: 2698438.4,
+      oldrate: 2448180
+    }
+  };
+  differenceinrate(2698438.4,2448180);
+  
+  var comparison ={
+    monthMarch:{
+      newrate: 2884537.6,
+      oldrate: 2617020
+    }
+  };
+  differenceinrate(2884537.6,2617020);
+  
+  var comparison ={
+    monthApril:{
+      newrate: 2791488,
+      oldrate: 2532600
+    }
+  };
+  differenceinrate(2791488,2532600);
+  
+  var comparison ={
+    monthMay:{
+      newrate: 2884537.6,
+      oldrate: 2617020
+    }
+  };
+  differenceinrate(2884537.6,2617020);
+  
+  var comparison ={
+    monthJune:{
+      newrate: 2791488,
+      oldrate: 2532600
+    }
+  };
+  differenceinrate(2791488,2532600);
+  
+  var comparison ={
+    monthJuly:{
+      newrate: 2884537.6,
+      oldrate: 2617020
+    }
+  };
+  differenceinrate(2884537.6,2617020);
+  
+  var comparison ={
+    monthAugust:{
+      newrate: 2884537.6,
+      oldrate: 2617020
+    }
+  };
+  differenceinrate(2884537.6,2617020);
+  
+  var comparison ={
+    monthSeptember:{
+      newrate: 2791488,
+      oldrate: 2532600
+    }
+  };
+  differenceinrate(2791488,2532600);
+  
+  var comparison ={
+    monthOctober:{
+      newrate: 2884537.6,
+      oldrate: 2617020
+    }
+  };
+  differenceinrate(2884537.6,2617020);
+  
+  var comparion ={
+    monthNovember:{
+      newrate: 2791488,
+      oldrate: 2532600
+    }
+  };
+  differenceinrate(2791488,2532600);
+  
+  var comparison ={
+    monthDecember:{
+      newrate: 2884537.6,
+      oldrate: 2617020
+    }
+  };
+  differenceinrate(2884537.6,2617020);
